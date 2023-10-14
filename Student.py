27bc6356ec91c1c799cc122d19c39c31785f723d
Natalia Oleynik <110234719+ons24081985@users.noreply.github.com>
2023-10-14 16:29:13 +0200

    Student.py

---
+++ Student.py
@@ -0,0 +1,10 @@
+class Student:
+    print("Hi, I`m a student")
+    def __init__(self, name):
+        self.name = name
+
+nick= Student(name="Nick")
+ann = Student(name"Ann")
+
+print(nick.name)
+print(ann.name)
